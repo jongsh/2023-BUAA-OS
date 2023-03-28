feb24233bff0ba6d5bc86416e8be6974c5395034
陈俊华 <21373307@buaa.edu.cn>
2023-03-28 11:45:24 +0800

    lab3 first

--- kern/sched.c
+++ kern/sched.c
@@ -35,5 +35,18 @@ void schedule(int yield) {
 	 *   'TAILQ_FIRST', 'TAILQ_REMOVE', 'TAILQ_INSERT_TAIL'
 	 */
 	/* Exercise 3.12: Your code here. */
+	if (yield != 0 || count == 0 || e == NULL || e->env_status != ENV_RUNNABLE) {
+		panic_on(TAILQ_EMPTY(&env_sched_list));
 
+		if (e == NULL) {
+		} else if ( e->env_status != ENV_RUNNABLE) {
+			TAILQ_REMOVE(&env_sched_list, e, env_sched_link);
+		} else if (e != NULL) {
+			TAILQ_INSERT_TAIL(&env_sched_list, e, env_sched_link);
+		}
+
+		e = TAILQ_FIRST(&env_sched_list);
+		count = e->env_pri;
+	} 
+	env_run(e);
 }
